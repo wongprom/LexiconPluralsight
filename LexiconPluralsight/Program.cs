2150af36c52e26ea532df9963b0ac5d138caf8ff
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-05-20 21:06:36 +0200

    Add project files.

---
+++ LexiconPluralsight/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LexiconPluralsight;
+
+Console.WriteLine("Hello, World!");
+
+int amount = 23;
+int months = 10;
+int yearlyWage = Utilities.CalculateYearlyWage(amount, months);
+
+Console.WriteLine($"Yearly wage : {yearlyWage}");
+Console.ReadLine();
+
---
+++ LexiconPluralsight/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconPluralsight
+{
+    internal class Utilities
+    {
+        public static int CalculateYearlyWage(int amount, int months, int bonus = 0)
+        {
+            return amount * months + bonus;
+        }
+    }
+}
