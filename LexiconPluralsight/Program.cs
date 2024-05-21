95d689dc4f9f94a2961d37dc5d3196067ed6b90f
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-05-21 11:11:02 +0200

    New Instanciate of Employee, jhon.

--- LexiconPluralsight/Program.cs
+++ LexiconPluralsight/Program.cs
@@ -3,8 +3,6 @@
 Console.WriteLine("Create an employee");
 Console.WriteLine("-----------------");
 Console.WriteLine();
-
-//Instaciate employee
 Employee jane = new Employee("Jane", "Doe", new DateTime(1950, 1, 30), 25);
 
 jane.DisplayEmployeeDetails();
@@ -14,3 +12,15 @@ jane.PerformWork();
 jane.PerformWork(12);
 double recivedWageJane = jane.ReceiveWage(true);
 Console.WriteLine($"Wage paid (message from Program): {recivedWageJane}");
+
+Console.WriteLine("Create an employee");
+Console.WriteLine("-----------------");
+Console.WriteLine();
+//Instanciate
+Employee john = new("John", "Doe", new DateTime(1990, 4, 28), 50);
+john.DisplayEmployeeDetails();
+john.PerformWork(4);
+john.PerformWork(6);
+john.PerformWork(2);
+double recivedWageJohn =  john.ReceiveWage(true);
+Console.WriteLine($"Wage paid (message from Program): {recivedWageJohn}");
