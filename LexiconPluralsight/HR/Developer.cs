8d62da03c6ee0653469759da9c28a4d489767c3e
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-05-27 15:26:02 +0200

    Create a Base and Derived Class
    Create child Developer class from parent Employee.
    Add private Field and a Property for that filed.

---
+++ LexiconPluralsight/HR/Developer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconPluralsight.HR
+{
+    internal class Developer : Employee
+    {
+
+        private string currentProject;
+
+        public string CurrentProject
+        {
+            get { return currentProject; } 
+            set 
+            { 
+                currentProject = value;            
+            }
+        }
+        public Developer(string firstName, string lastName, DateTime birthday, double? hourlyrate) : base(firstName, lastName, birthday, hourlyrate)
+        {
+        }
+    }
+}
