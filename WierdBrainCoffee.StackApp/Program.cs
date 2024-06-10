5573c53357cd4e4743cbc9bb6bd21d86c8accedf
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-06-10 11:18:10 +0200

    GENERICS
    Implement a Stack class for doubles.

---
+++ WierdBrainCoffee.StackApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace WierdBrainCoffee.StackApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new SimpelStack();
+            stack.Push(1.2);
+            stack.Push(2.8);
+            stack.Push(3.0);
+
+            double sum = 0.0;
+            while (stack.Count > 0)
+            {
+                double item = stack.Pop();
+                Console.WriteLine($"Item: {item}");
+                sum += item;
+            }
+            Console.WriteLine($"Sum: {sum}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WierdBrainCoffee.StackApp/SimpelStack.cs
@@ -0,0 +1,16 @@
+﻿namespace WierdBrainCoffee.StackApp
+{
+    public class SimpelStack
+    {
+        private readonly double[] _items;
+        private int _currentIndex = -1;
+
+        public SimpelStack() => _items = new double[10];
+
+        public int Count => _currentIndex + 1;
+        
+        public void Push(double item) =>_items[++_currentIndex] = item;
+
+        public double Pop() => _items[_currentIndex--];
+    }
+}
