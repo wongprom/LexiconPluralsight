ec797936652a1aa44a0297fa2bce62d11142aa66
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-06-10 15:21:40 +0200

    Create Console Application.
    Create classes Employee, EmployeeRepository to handle Add and Save new Employees.

---
+++ WiredBrainCoffee.StorageAp/Entities/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WiredBrainCoffee.StorageAp.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? FirstName{ get; set; }
+
+        public override string ToString() => $"Id: {Id}, FirstName: {FirstName}";
+    }
+}
---
+++ WiredBrainCoffee.StorageAp/Program.cs
@@ -0,0 +1,19 @@
+﻿using WiredBrainCoffee.StorageAp.Entities;
+using WiredBrainCoffee.StorageAp.Repositories;
+
+namespace WiredBrainCoffee.StorageAp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeRepository = new EmployeeRepository();
+            employeeRepository.Add(new Employee {FirstName = "Jimmy" });
+            employeeRepository.Add(new Employee {FirstName = "Sara" });
+            employeeRepository.Add(new Employee {FirstName = "Tom" });
+            employeeRepository.Save();
+
+            Console.ReadLine();
+        }
+    }
+}
